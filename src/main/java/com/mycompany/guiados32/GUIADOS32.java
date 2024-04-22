a3504da16adaf4c5e23e9e75d705865cef6dc364
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-04-21 20:09:33 -0500

    primer GUIA32

---
+++ src/main/java/com/mycompany/guiados32/GUIADOS32.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guiados32;
+
+/**
+ *
+ * @author nadir
+ */
+public class GUIADOS32 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
